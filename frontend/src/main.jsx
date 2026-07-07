a2db486e7802d25c85404f876ea0659d465c6b9d
Ronald de Lima Alves <ronald@MacBook-Air-de-Ronald.local>
2026-07-07 14:09:51 -0300

    feat: interface dinamica com json criada

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import Login from "./pages/Login.jsx"
 import { BrowserRouter , createBrowserRouter, RouterProvider } from "react-router-dom";
+import Feed from './pages/Feed.jsx'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     Component: Login
+  },
+  {
+    path: "/feed",
+    Component: Feed
   }
 ])
 
