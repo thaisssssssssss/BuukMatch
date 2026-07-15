3397a4def35c804ada903df8fa72743c905bdf8a
Ronald de Lima Alves <ronald@MacBook-Air-de-Ronald.local>
2026-07-15 16:12:01 -0300

    feat: novas implementacoes profile

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -24,52 +24,6 @@ import { ToastContainer, Bounce, toast } from 'react-toastify';
 import ToastMatch from '../components/ToastMatch';
 
 function Feed() {
-
-
-
-    // const initialPosts = [
-    //         {
-    //             "id": 1,
-    //             "title": "The Alchemist",
-    //             "author": "Paulo Coelho",
-    //             "description": "A mystical story about following your dreams and listening to your heart, following the journey of a young shepherd named Santiago.",
-    //             "pageCount": 208,
-    //             "publicationYear": 1988,
-    //             "publisher": "HarperOne",
-    //             "imgSrc": "/books/the_alchemist.jpg"
-    //         },
-    //         {
-    //             "id": 2,
-    //             "title": "To Kill a Mockingbird",
-    //             "author": "Harper Lee",
-    //             "description": "A gripping, heart-wrenching, and wholly remarkable tale of coming-of-age in a South poisoned by virulent prejudice.",
-    //             "pageCount": 324,
-    //             "publicationYear": 1960,
-    //             "publisher": "J. B. Lippincott & Co.",
-    //             "imgSrc": "/books/to_kill_a_mockingbird.jpg"
-    //         },
-    //         {
-    //             "id": 3,
-    //             "title": "1984",
-    //             "author": "George Orwell",
-    //             "description": "A dystopian social science fiction novel and cautionary tale about the dangers of totalitarianism, mass surveillance, and repressive regimentation.",
-    //             "pageCount": 328,
-    //             "publicationYear": 1949,
-    //             "publisher": "Secker & Warburg",
-    //             "imgSrc": "/books/1984.jpeg"
-
-    //         },
-    //         {
-    //             "id": 4,
-    //             "title": "The Hobbit",
-    //             "author": "J.R.R. Tolkien",
-    //             "description": "A classic fantasy novel following the quest of home-loving hobbit Bilbo Baggins to win a share of the treasure guarded by Smaug the dragon.",
-    //             "pageCount": 310,
-    //             "publicationYear": 1937,
-    //             "publisher": "George Allen & Unwin",
-    //             "imgSrc": "/books/the_hobbit.jpeg"
-    //         }
-    //     ]
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import NavBarApp from "../components/NavBarApp"
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft, X } from "lucide-react"
 import { useState } from "react"
 import { postService } from "../services/post"
 import "./styles/Profile.css"
@@ -46,13 +46,25 @@ function Profile() {
             {showPopUp && (
                 <div className='profile-pop-up-container'>
                     <div className='profile-pop-up-content'>
+                        <button className="profile-close-button"><X color="black"/></button>
                         <h2 className="profile-pop-up-title">Meus posts</h2>
                         <ul className="profile-post-list">
                             {
                                 userPosts.map((post) => (
-                                    <li className="">{post.description}</li>
+                                    <li className="profile-post-card" key={post.id}>
+                                        <div className="profile-post-image-container">
+                                            <img 
+                                                className="profile-post-img" 
+                                                src={`data:image/jpeg;base64,${post.photo}`}
+                                                alt="Capa do post" 
+                                            />
+                                        </div>
+                                        <div className="profile-post-info">
+                                            <h3 className="profile-post-title">{post.book.title}</h3>
+                                            <p className="profile-post-description">{post.legend}</p>
+                                        </div>
+                                    </li>
                                 ))
-
                             }
                         </ul>
                     </div>
